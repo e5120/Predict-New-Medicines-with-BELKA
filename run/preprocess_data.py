2ba3b82724eb372c5a3bb94185a4b7a125d446d9
e5120 <e5120@gm.kochi-ct.jp>
2024-04-27 08:10:37 +0900

    update: 前処理データを事前にシャッフル&保存先ディレクトリを新しく作成するように変更

--- run/preprocess_data.py
+++ run/preprocess_data.py
@@ -10,10 +10,15 @@ import lb.preprocessor
 @hydra.main(config_path="conf", config_name="preprocess_data", version_base=None)
 def main(cfg):
     # 既存ファイルのチェック
+    output_dir = Path(cfg.output_dir, cfg.preprocessor.prefix)
+    output_dir.mkdir(parents=True, exist_ok=True)
     basename = f"{cfg.preprocessor.prefix}_{cfg.stage}"
-    if len(list(Path(cfg.output_dir).glob(f"{basename}_*.npy"))):
+    exist_files = list(output_dir.glob(f"{basename}_*.npy"))
+    if len(exist_files):
         if cfg.overwrite:
             print(f"overwrite: {basename}")
+            for filename in exist_files:
+                filename.unlink()
         else:
             print(f"already exists: {basename}")
             return
@@ -22,14 +27,14 @@ def main(cfg):
         Path(cfg.data_dir, f"processed_{cfg.stage}.parquet"),
         columns=cfg.use_cols,
     )
-    df = df.with_columns(pl.int_range(len(df)).cast(pl.UInt32).alias("id"))
     if cfg.n_rows:
-        df = df.sample(n=cfg.n_rows, seed=42)
+        df = df.sample(n=cfg.n_rows, seed=cfg.seed)
+    df = df.select(pl.all().shuffle(seed=cfg.seed))
     # 前処理の適用
     preprocessor = getattr(lb.preprocessor, cfg.preprocessor.name)(cfg)
     data_gen = preprocessor.apply(df)
     for i, data in enumerate(data_gen):
-        np.save(Path(cfg.output_dir, f"{basename}_{i}.npy"), data)
+        np.save(Path(output_dir, f"{basename}_{i}.npy"), data)
 
 
 if __name__=="__main__":
